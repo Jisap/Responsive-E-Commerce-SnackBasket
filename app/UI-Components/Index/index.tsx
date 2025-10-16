1096b829f2eddc37c4d60b5d1e85ba7968a677cc
Jorge Matos <jisapjisap@gmail.com>
2025-10-16 19:16:57 +0200

    Add Category component with image slider to Index; integrate categories data for display

--- app/UI-Components/Index/index.tsx
+++ app/UI-Components/Index/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './Header/Hero'
+import Category from './Categories/Category'
 
 const Index = () => {
   return (
     <>
       <Hero />
+      <Category />
     </>
   )
 }
