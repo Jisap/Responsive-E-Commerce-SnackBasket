ee725620206581c61767fbb2217912fee3971a88
Jorge Matos <jisapjisap@gmail.com>
2025-10-16 19:53:04 +0200

    Add Banners component to Index and integrate it into the layout

--- app/UI-Components/Index/index.tsx
+++ app/UI-Components/Index/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './Header/Hero'
 import Category from './Categories/Category'
+import Banners from './Promotion-Banner/Banners'
 
 const Index = () => {
   return (
     <>
       <Hero />
       <Category />
+      <Banners />
     </>
   )
 }
