ae447c8e57f3a41708bb98186428d8c11c9219a7
Jorge Matos <jisapjisap@gmail.com>
2025-10-18 19:02:54 +0200

    Update Deals component with new image imports, enhance product display, and add product grid layout for better user interaction

--- app/UI-Components/Index/Deals/Deals.tsx
+++ app/UI-Components/Index/Deals/Deals.tsx
@@ -7,8 +7,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
 
-import Deal1 from "@/public/Deals-bg1.png";
-import Deal2 from "@/public/Deals-bg2.png";
+import Deal1 from "@/public/Deals-img1.png";
+import Deal2 from "@/public/Deals-img2.png";
 
 import toast from "react-hot-toast";
 
@@ -39,15 +39,16 @@ const dealsData: DealItem[] = [
 ];
 
 import products from "@/app/JsonData/BestDeals.json";
+import Link from "next/link";
 
 const Deals = () => {
-  
+
   const handleAddToCart = (product: any) => {
     let cart = JSON.parse(localStorage.getItem("cart") || "[]"); // Obtener el carro de la sesión
-  
+
     const existingProduct = cart.fin((item: any) => item.Id === product.Id); // Comprobar si el producto ya existe en el carro
-  
-    if(existingProduct) {
+
+    if (existingProduct) {
       toast(`${product.title} is already in the cart!`, {
         icon: <i className="bi bi-exclamation-triangle-fill text-yellow-500 text-2xl"></i>,
         style: {
@@ -58,13 +59,13 @@ const Deals = () => {
         }
       })
     } else {
-      cart.push({...product, qty: 1});                    // Agregar el producto al carro
+      cart.push({ ...product, qty: 1 });                    // Agregar el producto al carro
       localStorage.setItem("cart", JSON.stringify(cart)); // Actualizar el carro en el almacenamiento local
       window.dispatchEvent(new Event("storageUpdate"));   // Lanza evento de actualización de almacenamiento
 
       toast(`${product.title} added to the cart!`);
     }
-  }  
+  }
 
   return (
     <div className="px-[8%] lg:px-[12%] py-10">
@@ -91,8 +92,10 @@ const Deals = () => {
         {dealsData.map((deal, index) => (
           <SwiperSlide key={index}>
             <div
-              className={`deals-wrap px-5 py-6 h-[400px] md:h-[250px] rounded-2xl flex flex-col lg:flex-row justify-between items-center ${deal.className || ""
-                } gap-8 `}
+              className={`
+                deals-wrap px-5 py-6 h-[400px] md:h-[250px] rounded-2xl flex flex-col lg:flex-row justify-between items-center 
+                ${deal.className || ""} gap-8 `
+              }
             >
               <div className="w-full lg:w-1/2 deal-image">
                 <Image
@@ -108,9 +111,11 @@ const Deals = () => {
                 <h2 className=" Merienda text-4xl leading-11 whitespace-pre-line font-bold">
                   {deal.title}
                 </h2>
+
                 <p className="my-2 text-gray-800 font-normal">
                   {deal.description}
                 </p>
+                
                 <button className="px-5 py-2 cursor-pointer rounded-full text-white bg-prim font-bold hover:bg-prim-light hover:text-prim transition-all duration-500">
                   Shop Now <i className="bi bi-cart3 ms-0 md:ms-3"></i>
                 </button>
@@ -119,6 +124,73 @@ const Deals = () => {
           </SwiperSlide>
         ))}
       </Swiper>
+
+      <div className="my-20">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5">
+          {products.map((product, index) => (
+            <div
+              key={index}
+              className="product-wrap border border-gray-300 rounded-lg p-4 bg-white shadow-sm hover:shadow-md transition-all hover:border-prim cursor-pointer duration-300"
+            >
+              <div className="relative flex justify-center items-center w-full h-50">
+                <Image
+                  src={product.image}
+                  alt={product.title}
+                  width={180}
+                  height={180}
+                  className="object-contain mt-10"
+                />
+
+                <div
+                  onClick={() => handleAddToCart(product)}
+                  className="absolute top-0 right-0 flex justify-between items-center mt-1"
+                >
+                  <button className="px-4 py-2 cursor-pointer font-semibold text-[var(--prim-color)] bg-[var(--prim-light)] rounded-full text-md hover:bg-[var(--prim-color)] hover:text-white transition-all duration-500">
+                    Add <i className="bi bi-cart"></i>
+                  </button>
+                </div>
+              </div>
+
+              <Link
+                href={{
+                  pathname: "/UI-Components/Shop",
+                  query: { id: product.Id },
+                }}
+              >
+                <div className="space-y-1 mt-5 product-info">
+                  <div className="flex ite gap-2">
+                    <span className="text-gray-500 text-sm line-through">
+                      {product.lessprice}
+                    </span>
+
+                    <span className="text-xl font-semibold ">
+                      {product.price}{" "}
+                      <span className="text-gray-500 text-sm">/Qty</span>{" "}
+                    </span>
+                  </div>
+
+                  <span className="flex items-center text-yellow-500 text-md">
+                    <i className="bi bi-star-fill me-1"></i> {product.review}
+                  </span>
+
+                  <h2 className="text-lg font-normal Unbounded my-2 hover:text-[var(--prim-color)] transition-all duration-500">
+                    {product.title}
+                  </h2>
+
+                  <p className="text-md text-gray-500 flex items-center gap-1">
+                    <i className="bi bi-shop text-[var(--prim-color)]"></i> By
+                    Lucky SuperMarket
+                  </p>
+
+                  <p className="mt-3 Unbounded text-sm text-gray-600">
+                    Sold: {product.sold}
+                  </p>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
