51ec97f8b6a0eed4155014ac7186e42f234bd5af
Jorge Matos <jisapjisap@gmail.com>
2025-10-22 15:08:12 +0200

    Add detailed cart content display with itemized table, including quantity adjustment and subtotal calculation. Enhance user experience with empty cart notification and item removal functionality.

--- app/UI-Components/Pages/cart/page.tsx
+++ app/UI-Components/Pages/cart/page.tsx
@@ -81,6 +81,88 @@ const Cart = () => {
           </div>
         </div>
       </div>
+
+      {/* Cart Content */}
+      <div className="px-[8%] lg:px-[12%] py-10">
+        {cartItems.length === 0 ? (
+          <p className="text-lg bg-red-200 px-5 py-2 rounded">
+            Your Cart is Empty!
+          </p>
+        ):(
+          <>
+            {/* Desktop Table */}
+              <div className=" hidden md:flex gap-5">
+                <div className="overflow-x-auto">
+                  <table className="min-w-full border border-gray-300 rounded">
+                    <thead className="bg-[var(--prim-light)]">
+                      <tr>
+                        <th className="px-4 py-3 text-left">Product</th>
+                        <th className="px-4 py-3 text-left">Price</th>
+                        <th className="px-4 py-3 text-left">Quantity</th>
+                        <th className="px-4 py-3 text-left">SubTotal</th>
+                        <th className="px-4 py-3 text-left">Remove</th>
+                      </tr>
+                    </thead>
+
+                    <tbody>
+                      {cartItems.map((item) => {   
+                        const quantity = item.qty ?? 1;
+                        const priceNum = parseFloat(item.price.replace(/[^0-9.]+/g, "")) || 0;
+                        const itemSubTotal = priceNum * quantity;
+
+                        return (
+                          <tr
+                            key={item.Id}
+                            className="border-b border-gray-300 hover:bg-gray-100"
+                          >
+                            <td className="px-4 py-3 flex items-center gap-2">
+                              <img
+                                src={item.image}
+                                alt={item.title}
+                                className="w-16 h-16 object-contain rounded"
+                              />
+                              <span>{item.title}</span>
+                            </td>
+
+                            <td className="px-4 py-3">{item.price}</td>
+
+                            <td className="px-4 py-3">
+                              <input
+                                type="number"
+                                min={1}
+                                value={quantity}
+                                onChange={(e) =>
+                                  handleQtyChange(
+                                    item.Id,
+                                    parseInt(e.target.value)
+                                  )
+                                }
+                                className="w-16 border rounded px-1"
+                              />
+                            </td>
+
+                            <td className="px-4 py-3">
+                              {itemSubTotal.toFixed(2)}
+                            </td>
+
+                            <td className="px-4 py-3">
+                              <button
+                                onClick={() => handleRemove(item.Id)}
+                                className=" w-full bg-red-500/5 text-red-700 px-4   py-2 rounded cursor-pointer">
+                                Remove
+                              </button>
+                            </td>
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+          </>
+        )}
+      </div>
+     
     </>
   )
 }
